ff6296545099afa0adf5d97bd13f14e902432971
Jundulloh Rizki Ananda <99571736+JundiLesmana@users.noreply.github.com>
2026-09-05 00:38:01 +0700

    Add selfieStorageKey column to users table
    Add selfieStorageKey column for admin KYC registrations.

--- prisma/migrations/20260904010000_add_admin_kyc_selfie/migration.sql
+++ prisma/migrations/20260904010000_add_admin_kyc_selfie/migration.sql
@@ -1,3 +1,1 @@
--- Selfie storage key for platform admin KYC registrations.
--- The selfie is synced to the user profile image (User.image) upon approval.
 ALTER TABLE "users" ADD COLUMN "selfieStorageKey" TEXT;
